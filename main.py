77d7618959fd175ea856a64ee0f3cea7cafe95c6
Cruz Braden <brcruz@titanamerica.com>
2025-07-22 09:40:59 -0400

    Remove .env from tracking

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
 
 # LoginManager
 login_manager = LoginManager()
@@ -20,7 +25,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///users.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
